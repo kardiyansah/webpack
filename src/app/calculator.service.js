5e2c4f229c5a98396db2cdc9c7be3ce78efa87d3
kardiyansah <kardidragneel@gmail.com>
2025-03-31 16:02:18 +0700

    add branch common-js-module

--- src/app/calculator.service.js
+++ src/app/calculator.service.js
@@ -1,9 +1,9 @@
-class CalculatorService {
+module.exports = class CalculatorService {
   constructor() {
-    this.operand1 = document.querySelector('#operand1');
-    this.operand2 = document.querySelector('#operand2');
-    this.tombolTambah = document.querySelector('#tombol-tambah');
-    this.hasil = document.querySelector('#hasil');
+    this.operand1 = document.querySelector("#operand1");
+    this.operand2 = document.querySelector("#operand2");
+    this.tombolTambah = document.querySelector("#tombol-tambah");
+    this.hasil = document.querySelector("#hasil");
   }
 
   getInput() {
@@ -11,10 +11,13 @@ class CalculatorService {
   }
 
   setResult(angka1, angka2) {
+    if (angka1 == "") {
+      return (this.hasil.innerHTML = "");
+    }
     this.hasil.innerText = angka1 + angka2;
   }
 
   onClick(cb) {
-    this.tombolTambah.addEventListener('click', cb);
+    this.tombolTambah.addEventListener("click", cb);
   }
-}
+};
