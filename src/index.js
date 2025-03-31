b91a95bf2c6d10204353121eb02c64543453fcc0
kardiyansah <kardidragneel@gmail.com>
2025-03-31 16:37:22 +0700

    add branch ecma-scrip harmony

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { AlertService } from "./app/alert.service.js";
+import { CalculatorService } from "./app/calculator.service.js";
+import { JokesService } from "./app/jokes.service.js";
+
+import { run } from "./app/app.js";
+
+const alertService = new AlertService();
+const calculatorService = new CalculatorService();
+const jokesService = new JokesService();
+
+run(alertService, calculatorService, jokesService);
